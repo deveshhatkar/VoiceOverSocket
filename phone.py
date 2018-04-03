e17eb806cfc53d258028931cf06cd1ea83b03871
deveshhatkar <deveshhatkar@gmail.com>
2018-04-04 04:03:44 +0530

    Modified phone.py

--- phone.py
+++ phone.py
@@ -14,9 +14,9 @@ class Phone:
     audio = pyaudio.PyAudio()
 
     #Socket Initializations
-    connecting_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     connected_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    IP_ADDRESS = "192.168.0.103"
+    IP_ADDRESS = "localhost"
     PORT_NUMBER = 4040
 
     #Sound Frames storage variables
@@ -31,28 +31,36 @@ class Phone:
     #determines either sending or recieving
     flag = 0
 
-    def connect_as_server(self):
-        self.connecting_socket.bind((self.IP_ADDRESS, self.PORT_NUMBER))
-        self.connecting_socket.listen(1)
-        self.connected_socket = self.connecting_socket.accept()
-        print("\nThis is a server")
-
-    def connect_as_client(self):
-        try:
-            connected_socket.connect((self.IP_ADDRESS, self.PORT_NUMBER))
-            print("\nThis is a client")
-        except:
-            print("\nThis is a server")
+##    def connect_as_server(self):
+##        try:
+##            self.connecting_socket.bind((self.IP_ADDRESS, self.PORT_NUMBER))
+##            self.connecting_socket.listen(1)
+##            self.connected_socket = self.connecting_socket.accept()
+##            print("\nThis is a server")
+##        except:
+##            print("\nThis is a client")
+##            
+##        
+##
+##    def connect_as_client(self):
+##        try:
+##            connected_socket.connect((self.IP_ADDRESS, self.PORT_NUMBER))
+##            print("\nThis is a client")
+##        except:
+##            print("\nThis is a server")
 
     def __init__(self):
 
         #Socket Connection code
-        thread_server = threading.Thread(thread=self.connect_as_server())
-        thread_client = threading.Thread(thread=self.connect_as_client())
-
-        thread_server.start()
-        thread_client.start()        
-
+        self.server_socket.bind((self.IP_ADDRESS, self.PORT_NUMBER))
+        self.server_socket.listen(5)
+        (self.connected_socket, address) = self.server_socket.accept()
+##        thread_server = threading.Thread(self.connect_as_server())
+##        thread_client = threading.Thread(self.connect_as_client())
+##
+##        thread_client.start()
+##        thread_server.start()                
+        print("This is Server")
         print("Hello World!")
 
     def main(self):
