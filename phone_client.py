41f62b8061905d3d5887edfe3c55e67091ed30bd
Vedaant Joshi <vedaant912@gmail.com>
2018-04-04 04:30:15 +0530

    phone_client.py Updated

--- phone_client.py
+++ phone_client.py
@@ -16,8 +16,8 @@ class Phone:
     #Socket Initializations
     #server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     connected_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    IP_ADDRESS = "localhost"
-    PORT_NUMBER = 4040
+    IP_ADDRESS = "192.168.0.103"
+    PORT_NUMBER = 40404
 
     #Sound Frames storage variables
     frames = []
@@ -31,41 +31,17 @@ class Phone:
     #determines either sending or recieving
     flag = 0
 
-##    def connect_as_server(self):
-##        try:
-##            self.connecting_socket.bind((self.IP_ADDRESS, self.PORT_NUMBER))
-##            self.connecting_socket.listen(1)
-##            self.connected_socket = self.connecting_socket.accept()
-##            print("\nThis is a server")
-##        except:
-##            print("\nThis is a client")
-##            
-##        
-##
-##    def connect_as_client(self):
-##        try:
-##            connected_socket.connect((self.IP_ADDRESS, self.PORT_NUMBER))
-##            print("\nThis is a client")
-##        except:
-##            print("\nThis is a server")
-
     def __init__(self):
 
         #Socket Connection code
-        #self.connected_socket.bind((self.IP_ADDRESS, self.PORT_NUMBER))
 
         self.connected_socket.connect((self.IP_ADDRESS, self.PORT_NUMBER))
 
-##        thread_server = threading.Thread(self.connect_as_server())
-##        thread_client = threading.Thread(self.connect_as_client())
-##
-##        thread_client.start()
-##        thread_server.start()                
-        print("This is Server")
+        print("This is Client!!!")
         print("Hello World!")
 
     def main(self):
-        obj = Phone()
+        print("Hello")
     
 
 if __name__ == '__main__':
