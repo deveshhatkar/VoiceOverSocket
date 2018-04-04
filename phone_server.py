18abd7429604e89246d1fdd42a3603418b0f3dc9
deveshhatkar <deveshhatkar@gmail.com>
2018-04-04 08:19:19 +0530

    First complete "phone_server.py"

--- phone_server.py
+++ phone_server.py
@@ -3,6 +3,7 @@ import pyaudio
 import wave
 import threading
 
+#================================================================================
 class Phone:
 
     #Initialiations for Microphone
@@ -19,9 +20,14 @@ class Phone:
     IP_ADDRESS = "192.168.0.103"
     PORT_NUMBER = 40404
 
-    #Sound Frames storage variables
-    frames = []
+    #Threading Initializations
+    recorder_thread = threading.Thread(self.recorder_thread())
+    receiver_thread = threading.Thread(self.receiver_thread())
 
+    #Sound Frames storage variables
+    sending_frames = []
+    receiving_frames = []
+    
     #Initializations for creating of 'wav' files
     waveFile = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
     waveFile.setnchannels(CHANNELS)
@@ -29,7 +35,7 @@ class Phone:
     waveFile.setframerate(RATE)
 
     #determines either sending or recieving
-    flag = 0 # 0-active recorder thread / 1-active sender thread
+    flag = 0 # 0-active receiver_thread() / 1-active recorder_thread()
 
     #----------------Methods-------------------
 
@@ -42,22 +48,29 @@ class Phone:
 
     #------------------------------------------
 
-    def on_key_press():
+    def on_key_press(key):
         #Verify that only 'G' is pressed otherwise do nothing.
-
         #This method sets the flag to 1. Indicating 'recorder_thread()'
             #becomes active.
+        try:
+            if(key.char == 'g'):
+                self.flag = 1        
 
     #------------------------------------------
 
-    def on_key_release():
+    def on_key_release(key):
         #Verify that only 'G' is released otherwise do nothing.
+        if(key.char == 'g'):
         
-        #This mehtod sets the flag to 0.
-        
-        #Send the frames recorded in 'recorder_thread()' method over the socket.        
-        
-        #send a special symbol over socket to trigger receiver code.
+            #This mehtod sets the flag to 0.
+            self.flag = 0
+            
+            #Send the frames recorded in 'recorder_thread()' method over the socket.
+            size = len(self.sending_frames)
+            for i in range(size):
+                self.connected_socket.send(self.sending_frames[i])
+
+            self.sendding_frames = [] # Emptying the sending_frames
 
     #-------------------------------------------
 
@@ -67,29 +80,75 @@ class Phone:
                             frames_per_buffer=CHUNK)
         print ("\nrecording...")
         while True:
-            while(flag == 0):
+            while(self.flag == 1):
                 #Code to capture the microphone using stream.read() method
                 data = stream.read(CHUNK)
-                self.frames.append(data)
+                self.sending_frames.append(data)
                 
 
     #-------------------------------------------
 
     def receiver_thread():
         while True:
-            while("Check if the special symbol has arrived over the socket"):
+            self.receiving_frames = [] #Emptying the receiving frames
+            while(self.connected_socket.recv(1) != ''):
 
                 #Receive all the frames using a while loop, breaking
                     #it when empty byte is recieved.
+                while True:
+                    text = self.connected_socket.recv(1024)
+                    self.receiving_frames.append(text)
+                    if text == '':
+                        break                    
 
                 #Make a 'wav' files using the recieved frames and save it.
+                self.waveFile.writeframes(b''.join(self.receiving_frames))
+                waveFile.close()
 
                 #Play the saved 'wav' file.
+                wf = wave.open(self.WAVE_OUTPUT_FILENAME, 'rb')
+
+                p = pyaudio.PyAudio()
+
+                stream = p.open(format = p.get_format_from_width(wf.getsampwidth()),
+                                channels = wf.getchannels(),
+                                rate=wf.getframerate(),
+                                output = True)
+
+                data = wf.readframes(self.CHUNK)
+
+                while data != '':
+
+                    stream.write(data)
+                    data = wf.readframes(self.CHUNK)
+
+                stream.close()
+                p.terminate()
+
+    #-------------------------------------------
+
+    def __del__(self):
+        self.recoder_thread.stop()
+        self.receiver_thread.stop()
 
     #-------------------------------------------
 
     def main(self):
+        #Create a 'Phone' object.
+
+        #starting the 'recorder_thread()'thread and 'receiver_thread()'thread
+        self.recoder_thread.start()
+        self.receiver_thread.start()
+
+        #Implement Keyboard Listener for listening to the 'G' key.
+        with keyboard.listener(
+            on_press=on_key_press,
+            on_release=on_key_release) as listener:
+            listener.join()
+            
         print("Hello")
+
+#================================================================================
         
 if __name__ == '__main__':
     Phone().main()
