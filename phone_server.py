ca5801aa4cc0b8cf8c5036b0512fbf8da9aad529
deveshhatkar <deveshhatkar@gmail.com>
2018-04-16 09:05:56 +0530

    Pushed Working Client and Server Files!

--- phone_server.py
+++ phone_server.py
@@ -2,152 +2,180 @@ import socket
 import pyaudio
 import wave
 import threading
+import keyboard
+import time
+from pynput.keyboard import Key, Listener
 
-#================================================================================
 class Phone:
 
-    #Initialiations for Microphone
+    #Initializations for Microphone.
     FORMAT = pyaudio.paInt16
     CHANNELS = 2
     RATE = 44100
-    CHUNK = 1024
+    CHUNK = 10240
     WAVE_OUTPUT_FILENAME = "file_client.wav"
     audio = pyaudio.PyAudio()
-
-    #Socket Initializations
+    
+    #Socket Initializations.
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     connected_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    IP_ADDRESS = "localhost"
-    PORT_NUMBER = 40400
-
-    #Sound Frames storage variables
+    IP_ADDRESS = "192.168.43.236"
+    PORT_NUMBER = 8181
+    
+    #frames storage variables
     sending_frames = []
     receiving_frames = []
     
-    #Initializations for creating of 'wav' files
-    waveFile = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-    waveFile.setnchannels(CHANNELS)
-    waveFile.setsampwidth(audio.get_sample_size(FORMAT))
-    waveFile.setframerate(RATE)
+    #Initializations for creating of 'wav' files.
+##    waveFile = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+##    waveFile.setnchannels(CHANNELS)
+##    waveFile.setsampwidth(au  xxxdio.get_sample_size(FORMAT))
+##    waveFile.setframerate(RATE)
+
     #determines either sending or recieving
-    flag = 0 # 0-active receiver_thread() / 1-active recorder_thread()
+    flag = -1       #0 - Recording / 1 - Recieving / '-1' - Nothing
 
-    #----------------Methods-------------------
+    #def _init_(self):
 
-    def __init__(self):
+        #Socket Exception Handling incase of refused connection
 
-        #Socket Connection code
+    
+    def on_key_press(self, key):
+        #Verify that only 'G' is pressed otherwise do nothing.
+        if(keyboard.is_pressed('space')):
+            self.flag = 0
+            print('Space Pressed')
+
+        #This method sets the flag to 0. Indicating 'recording' starts.
+            
+    def on_key_release(self, key):
+        pass
+        #Verify that only 'G' is released otherwise do nothing.
+##        if(keyboard.is_pressed('x')):
+##            print('X pressed')
+##            self.flag = 1
+##            #Send the frames recorded in 'recorder_thread()' method over the socket.
+##            size = len(self.sending_frames)
+##                
+##            for i in range(size):
+##                self.connected_socket.send(self.sending_frames[i])
+##
+##            print("\nframes sent")
+##
+##            self.sending_frames = [] #Emptying the sending_frames.
+        #send a special symbol over socket to trigger receiver code.
+
+    def keyboard_listener_thread(self):
+        #Implement Keyboard Listener for listening to the 'G' key.
+        with Listener(
+               on_press=self.on_key_press,
+               on_release=self.on_key_release) as listener:
+               listener.join()
+
+
+    def main(self):
+
+        #Socket Connection code. Server Side.
+        print("Waiting for Client to connect!")
         self.server_socket.bind((self.IP_ADDRESS, self.PORT_NUMBER))
         self.server_socket.listen(5)
         (self.connected_socket, address) = self.server_socket.accept()
+        print("\nConnected to Client")
 
-    #------------------------------------------
-
-    def on_key_press(key, self):
-        #Verify that only 'G' is pressed otherwise do nothing.
-        #This method sets the flag to 1. Indicating 'recorder_thread()'
-            #becomes active.
-        try:
-            if(key.char == 'g'):
-                self.flag = 1
-        except:
-            print ("a")
-    #------------------------------------------
-
-    def on_key_release(key, self):
-        #Verify that only 'G' is released otherwise do nothing.
-        if(key.char == 'g'):
+        #Setting up and Starting Keyboard Listener thread
+        thread = threading.Thread(target=self.keyboard_listener_thread)
+        thread.start()
         
-            #This mehtod sets the flag to 0.
-            self.flag = 0
-            
-            #Send the frames recorded in 'recorder_thread()' method over the socket.
-            size = len(self.sending_frames)
-            for i in range(size):
-                self.connected_socket.send(self.sending_frames[i])
-
-            self.sendding_frames = [] # Emptying the sending_frames
-
-    #-------------------------------------------
-
-    def recorder_thread(self):
-        stream = audio.open(format=FORMAT, channels=CHANNELS,
-                            rate=RATE, input=True,
-                            frames_per_buffer=CHUNK)
-        print ("\nrecording...")
+        
         while True:
-            while(self.flag == 1):
-                #Code to capture the microphone using stream.read() method
-                data = stream.read(CHUNK)
-                self.sending_frames.append(data)
+            time.sleep(.10)
+            print('Doing Nothing')
+
+            self.sending_frames = []
+            #Recorder Code
+            while(self.flag == 0):
+                #time.sleep(.10)
                 
+                
+                print('\nRecording...')
+                #Recorder Code Initializations.
+                stream = self.audio.open(format=self.FORMAT, channels=self.CHANNELS,
+                                    rate=self.RATE, input=True,
+                                    frames_per_buffer=self.CHUNK)
+                #Code to capture the microphone using stream.read() method
+                data = stream.read(self.CHUNK)
+                self.sending_frames.append(data)
+                if(keyboard.is_pressed('x')):
+                    print('X pressed')
+                    #Send the frames recorded in 'recorder_thread()' method over the socket.
+                    size = len(self.sending_frames)                        
+                    for i in range(size):
+                        self.connected_socket.send(self.sending_frames[i])
+                    print("\nframes sent")
+                    self.sending_frames = [] #Emptying the sending_frames.
 
-    #-------------------------------------------
+                    self.flag = -1
+                stream.close()
+                
+                    
+            self.sending_frames = []
 
-    def receiver_thread(self):
-        while True:
-            self.receiving_frames = [] #Emptying the receiving frames
-            while(self.connected_socket.recv(1) != ''):
-
+            self.connected_socket.setblocking(0)
+            #reciever code
+            while(True):
+                
+                try:
+                    text = self.connected_socket.recv(10)
+                    self.receiving_frames = []
+                    self.receiving_frames.append(text)
+                except socket.error:
+                    break
+                
+                
+                print("\n Recieving")
                 #Receive all the frames using a while loop, breaking
                     #it when empty byte is recieved.
                 while True:
-                    text = self.connected_socket.recv(1024)
-                    self.receiving_frames.append(text)
-                    if text == '':
-                        break                    
+                    try:
+                        text = self.connected_socket.recv(1024)
+                        self.receiving_frames.append(text)
+                    except socket.error:
+                        break
+                    
 
                 #Make a 'wav' files using the recieved frames and save it.
-                self.waveFile.writeframes(b''.join(self.receiving_frames))
+                waveFile = wave.open(self.WAVE_OUTPUT_FILENAME, 'wb')
+                waveFile.setnchannels(self.CHANNELS)
+                waveFile.setsampwidth(self.audio.get_sample_size(self.FORMAT))
+                waveFile.setframerate(self.RATE)
+
+
+                waveFile.writeframes(b''.join(self.receiving_frames))
                 waveFile.close()
 
                 #Play the saved 'wav' file.
                 wf = wave.open(self.WAVE_OUTPUT_FILENAME, 'rb')
-
                 p = pyaudio.PyAudio()
 
                 stream = p.open(format = p.get_format_from_width(wf.getsampwidth()),
-                                channels = wf.getchannels(),
+                                channels = wf.getnchannels(),
                                 rate=wf.getframerate(),
                                 output = True)
 
                 data = wf.readframes(self.CHUNK)
 
+                print("\nPlaying")
                 while data != '':
-
                     stream.write(data)
                     data = wf.readframes(self.CHUNK)
 
                 stream.close()
                 p.terminate()
+                wf.close()
+                self.flag = -1
 
-    #-------------------------------------------
-
-##    def __del__(self):
-##        self.recoder_thread.stop()
-##        self.receiver_thread.stop()
-
-    #-------------------------------------------
-
-    def main(self):
-        #Create a 'Phone' object.
-
-        #starting the 'recorder_thread()'thread and 'receiver_thread()'thread
-        thread1 = threading.Thread(self.recorder_thread)
-        thread2 = threading.Thread(self.receiver_thread)
-
-        thread1.start()
-        thread2.start()
-
-        #Implement Keyboard Listener for listening to the 'G' key.
-        with keyboard.listener(
-            on_press=on_key_press,
-            on_release=on_key_release) as listener:
-            listener.join()
-            
-        print("Hello")
-
-#================================================================================
+            self.connected_socket.setblocking(1)
         
+
 if __name__ == '__main__':
     Phone().main()
